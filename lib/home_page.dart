880302fba3ca43aa1568dafe132e83b726bbcead
kamlesh thavani <kamleshthavani12345@gmail.com>
2022-02-09 13:25:41 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  static const days = 30;
+  static const name = "Kamlesh";
+    
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+        appBar: AppBar(
+          title: Text("Catalog App"),
+        ),
+        body: Center(
+          child: Container(
+            child: const Text("Welcome to $days Days of flutter by $name"),
+          ),
+        ),
+        drawer: Drawer(),
+      );
+  }
+}
