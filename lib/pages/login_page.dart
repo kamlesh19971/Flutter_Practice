4ff049b5a78aa07c0dc00bcc109aba8fee1c623b
kamlesh thavani <kamleshthavani12345@gmail.com>
2022-02-09 22:52:49 +0530

    Day 6 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,11 +1,19 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/utils/routes.dart';
 
+import '../utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
   
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  String name = '';
+  bool changeButton = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -15,32 +23,71 @@ class LoginPage extends StatelessWidget {
           children: [
             Image.asset("assets/images/login_image.png", fit: BoxFit.cover),
             const SizedBox(height: 20),
-            const Text(
-              "Welcome",
-              style: TextStyle( fontSize: 24, fontWeight: FontWeight.bold),
+            Text(
+              "Welcome $name",
+              style: const TextStyle( fontSize: 24, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 20),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
               child: Column(
                 children:  [
-                  const TextField(
-                    decoration: InputDecoration( hintText: "Enter Username", labelText: "Username"),
+
+                  TextField(
+                    decoration: const InputDecoration( hintText: "Enter Username", labelText: "Username"),
+                    onChanged: (value) { 
+                      name = value;
+                      setState(() {});
+                    }
                   ),
+
                   const TextField(
                     obscureText: true,
-                    decoration: InputDecoration( hintText: "Enter Password", labelText: "Password", ),
+                    decoration: InputDecoration( hintText: "Enter Password", labelText: "Password"),
                   ),
       
                   const SizedBox(height: 40),
-      
-                  ElevatedButton(
-                    onPressed: () {
+
+                  InkWell(
+                    onTap: () async { 
+                      setState(() {
+                        changeButton = !changeButton;
+                      });
+
+                      await Future.delayed(Duration(seconds: 1));
                       Navigator.pushNamed(context, MyRoutes.homeRoute);
-                    }, 
-                    style: TextButton.styleFrom(minimumSize: Size(150, 50)),
-                    child: const Text("Login"),
+                    },
+
+                    child: AnimatedContainer(
+                      duration: const Duration(seconds: 1),
+                      width: changeButton ? 50 : 150,
+                      height: 40,
+                      alignment: Alignment.center,
+                      child: changeButton 
+                      ? const Icon(
+                          Icons.done, 
+                          color:  Colors.white
+                        )
+                      : const Text(
+                        "Login", 
+                        style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        // shape: changeButton  ? BoxShape.circle : BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(changeButton ? 50 : 8)
+                      ),                      
+                    ),
                   )
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   }, 
+                  //   style: TextButton.styleFrom(minimumSize: Size(150, 50)),
+                  //   child: const Text("Login"),
+                  // )
                 ]
               ),
             )
