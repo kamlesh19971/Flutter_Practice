cdfaf2b599b362b5daf4d42c70cce4ee33430248
kamlesh thavani <kamleshthavani12345@gmail.com>
2022-02-10 18:47:10 +0530

    DAy 17 completed

---
+++ gridview.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'package:flutter_catalog/models/catalog.dart';
+import 'package:flutter_catalog/widgets/drawer.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  static const days = 30;
+  static const name = "Kamlesh";
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async{
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData =  json.decode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map((item) => Item.fromMap(item))
+        .cast<Item>().toList();
+    
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+ 
+    return Scaffold( 
+        appBar: AppBar(
+          title: Text("Catalog App"),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: CatalogModel.items.isNotEmpty ? 
+          // ListView.builder(
+          //   itemCount: CatalogModel.items.length, 
+          //   itemBuilder: (BuildContext context, int index) => ItemWidget(item: CatalogModel.items[index]),
+          // )
+          GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 16,
+              crossAxisSpacing: 16
+            ), 
+            itemBuilder: (context, index) {
+              final item = CatalogModel.items[index];
+              return Card(
+                clipBehavior: Clip.antiAlias,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                child: GridTile(
+                  header: Container(
+                    child: Text(item.name, style: TextStyle(color: Colors.white),),
+                    padding: const EdgeInsets.all(12),
+                    decoration: const BoxDecoration(
+                      color: Colors.deepPurple
+                    ),
+                  ),
+                  child: Image.network(item.image),
+                  footer: Container(
+                    child: Text(item.price.toString(), style: TextStyle(color: Colors.white),),
+                    padding: const EdgeInsets.all(12),
+                    decoration: const BoxDecoration(
+                      color: Colors.black
+                    ),
+                  ),
+                )
+              );
+            }, 
+            itemCount: CatalogModel.items.length
+          )
+          : 
+          const Center(
+            child: CircularProgressIndicator(),
+          ),
+        ),
+        drawer: MyDrawer(),
+      );
+  }
+}
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -5,7 +5,7 @@ import 'package:google_fonts/google_fonts.dart';
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
         primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
+        fontFamily: GoogleFonts.poppins().fontFamily,
         appBarTheme: const AppBarTheme(
           color: Colors.white,
           elevation: 0.0,
@@ -19,4 +19,10 @@ class MyTheme {
   static ThemeData darkTheme(BuildContext context) => ThemeData(
     brightness: Brightness.dark
   );
+
+  // Colors
+
+  static Color creameColor = Color(0xfff5f5f5);
+  static Color darkBluisColor = Color(0xff403b58);
+
 }
